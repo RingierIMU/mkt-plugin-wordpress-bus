7a506ae2b98e899ec4a2086ebbbfdb275c7137b4
Wasseem K. (Ringier SA) <wasseemk@ringier.co.za>
2021-09-29 17:49:22 +0400

    feat: create structure for autoloading

--- wp-bus.php
+++ wp-bus.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * WP-Bus
+ *
  * @package RingiermuWPBus
  * @author Wasseem Khayrattee
  * @copyright 2021 Ringier
@@ -20,30 +21,34 @@
  * Text Domain: wp-bus
  * Domain Path: /languages
  *
+ *
  * reference: https://developer.wordpress.org/plugins/
-*/
-
-/*
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 */
 
 // Make sure we don't expose any info if called directly
 if (!function_exists('add_action')) {
-    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    header( 'Status: 403 Forbidden' );
+    header( 'HTTP/1.1 403 Forbidden' );
     exit;
 }
 
 define('WP_BUS_RINGIER_VERSION', '1.0.0');
 define('WP_BUS_RINGIER_MINIMUM_WP_VERSION', '4.0');
 define('WP_BUS_RINGIER_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('DS', DIRECTORY_SEPARATOR);
+
+//load our main file now
+require_once WP_BUS_RINGIER_PLUGIN_DIR . DS . 'src/wp-bus-main.php';
