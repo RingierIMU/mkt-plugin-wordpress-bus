2fe36e2f5f38438e491922f567b8961a8312843d
Wasseem K. (Ringier SA) <wasseemk@ringier.co.za>
2021-09-29 17:35:11 +0400

    feat: add composer file

--- wp-bus.php
+++ wp-bus.php
@@ -1,24 +1,49 @@
 <?php
 /**
  * WP-Bus
- * @package PluginPackage
- * @author Your Name
+ * @package RingiermuWPBus
+ * @author Wasseem Khayrattee
  * @copyright 2021 Ringier
  * @license GPL-2.0-or-later
  *
  * @wordpress-plugin
  * Plugin Name: WP-Bus
  * Plugin URI: https://github.com/wkhayrattee/wp-bus
- * Description: A plugin to push events to Ringier CDE via the BUS API whenever an article is created, updated or deleted.
+ * Description: A plugin to push events to Ringier CDE via the BUS API whenever an article is created, updated or deleted
  * Version: 1.0
- * Requires at least: 5.2
- * Requires PHP: 7.2
+ * Requires at least: 5.8
+ * Requires PHP: 7.4
  * Author: Wasseem Khayrattee
  * Author URI: https://github.com/wkhayrattee/
- * License: GPL2
+ * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wp-bus
  * Domain Path: /languages
  *
  * reference: https://developer.wordpress.org/plugins/
 */
+
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+*/
+
+// Make sure we don't expose any info if called directly
+if (!function_exists('add_action')) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
+
+define('WP_BUS_RINGIER_VERSION', '1.0.0');
+define('WP_BUS_RINGIER_MINIMUM_WP_VERSION', '4.0');
+define('WP_BUS_RINGIER_PLUGIN_DIR', plugin_dir_path(__FILE__));
