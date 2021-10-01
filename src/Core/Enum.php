953974003220742f3b7dc3ab5341bd0860ab0d9d
Wasseem K. (Ringier SA) <wasseemk@ringier.co.za>
2021-10-02 03:37:04 +0400

    feat: use enums

---
+++ src/Core/Enum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RingierBusPlugin;
+
+class Enum
+{
+    //ADMIN SETTINGS PAGE
+    const ADMIN_SETTINGS_PAGE_TITLE = 'Ringier Bus API Settings';
+    const ADMIN_SETTINGS_MENU_TITLE = 'Bus API';
+    const ADMIN_SETTINGS_MENU_SLUG = 'wp-bus-api';
+
+    //ADMIN LOG PAGE
+    const ADMIN_LOG_PAGE_TITLE = 'Bus API Message Log';
+    const ADMIN_LOG_MENU_TITLE = 'Message Log';
+    const ADMIN_LOG_MENU_SLUG = 'wp-bus-log';
+}
