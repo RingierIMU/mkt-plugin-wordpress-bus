841df3afbbc7c73a4d1c8e91098cff1aaf42e563
Wasseem K. (Ringier SA) <wasseemk@ringier.co.za>
2021-10-01 17:42:15 +0400

    feat: we will use this method to log our custom messages

--- src/ringier_bus_plugin_helper.php
+++ src/ringier_bus_plugin_helper.php
@@ -4,12 +4,17 @@ namespace RingierBusPlugin;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
+/**
+ * Wrapper to log Messages in a custom log file
+ * @param $message
+ * @throws \Exception
+ */
 function logthis($message)
 {
     $log = new Logger('ringier_bus_plugin_log');
     $stream = new StreamHandler(WP_CONTENT_DIR . DS . 'bus_plugin.log', Logger::INFO);
     $log->pushHandler($stream);
-//    $log->info($message);
+    $log->info($message);
     unset($log);
     unset($stream);
 }
