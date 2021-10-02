09777230a3c92dd3d5e84077321be8cbe6d4d4d4
Wasseem K. (Ringier SA) <wasseemk@ringier.co.za>
2021-10-02 04:59:45 +0400

    feat: add enums for options name

--- src/Core/Enum.php
+++ src/Core/Enum.php
@@ -6,6 +6,7 @@ class Enum
 {
     const PLUGIN_KEY = 'wp-bus';
     const SETTINGS_PAGE_OPTION_GROUP = 'wp_bus_settingspage_group';
+    const SETTINGS_PAGE_OPTION_NAME = 'wp_bus_settingspage_options';
     //ADMIN SETTINGS PAGE
     const ADMIN_SETTINGS_PAGE_TITLE = 'Ringier Bus API Settings';
     const ADMIN_SETTINGS_MENU_TITLE = 'Bus API';
