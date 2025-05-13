050810da56e4fdd888d07ce14a2fc365571d201f
Wasseem Khayrattee <hey@wk.contact>
2025-05-13 13:45:30 +0400

    SOL-1708 | Enum for the custom post type toggle field

--- src/Core/Enum.php
+++ src/Core/Enum.php
@@ -48,6 +48,8 @@ class Enum
     const FIELD_TEXT_ALTERNATE_PRIMARY_CATEGORY = 'field_text_alt_primary_category';
     const FIELD_GOOGLE_YOUTUBE_API_KEY = 'field_google_youtube_api_key';
     const FIELD_ENABLE_QUICK_EDIT = 'field_enable_quick_edit';
+    public const FIELD_ALLOW_CUSTOM_POST_TYPES = 'allow_custom_post_types'; // master toggle
+    public const FIELD_ENABLED_CUSTOM_POST_TYPE_LIST = 'enable_custom_post_type_events'; // specific post types
 
     //ADMIN LOG PAGE
     const ADMIN_LOG_PAGE_TITLE = 'Ringier Bus API Log';
