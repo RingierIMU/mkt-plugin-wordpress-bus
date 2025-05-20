2f61e13d6b315997308b74b94a5fc8b22c4afa6a
Wasseem Khayrattee <hey@wk.contact>
2025-05-20 18:12:17 +0400

    chore: SOL-954 | add a more precise description to help the user understand what we do with this author base text-field

--- views/admin/field-author-base.php
+++ views/admin/field-author-base.php
@@ -7,4 +7,6 @@ use RingierBusPlugin\Enum;
        name="<?php echo esc_attr(Enum::SETTINGS_PAGE_OPTION_NAME . '[' . $args['label_for'] . ']'); ?>"
        value="<?php echo esc_attr($args['value']); ?>"
        class="regular-text">
-<p class="description">Custom base slug for author URLs (e.g., "author" → "auteur").</p>
+<p class="description">
+    Specify the current author base slug used on your site (e.g., <code>author</code>, <code>auteur</code>, etc.). <br>This field does not change the base, it is used just for us to detect and construct the author URLs correctly.
+</p>
