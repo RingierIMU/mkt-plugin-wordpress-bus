7d6414e278f08aac32bed2ccb81f226a5c10a341
Wasseem Khayrattee <hey@wk.contact>
2025-05-24 16:01:35 +0400

    refactor: SOL-954 | remove unused blocks

--- src/ringier_bus_plugin_helper.php
+++ src/ringier_bus_plugin_helper.php
@@ -6,30 +6,7 @@
  *
  * @github wkhayrattee
  */
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
 use RingierBusPlugin\Enum;
-
-/**
- * Wrapper to log Messages in a custom log file
- * Note: This function will only work if you are using $_ENV['APP_ENV']
- * and if it is set to any other value than 'prod'
- *
- * @param $message
- *
- * @throws \Exception
- */
-function ringier_infologthis($message): void
-{
-    if (isset($_ENV['APP_ENV']) && ($_ENV['APP_ENV'] != 'prod')) {
-        $log = new Logger('ringier_bus_plugin_log');
-        $stream = new StreamHandler(WP_CONTENT_DIR . RINGIER_BUS_DS . Enum::RINGIER_LOG_FILE_MESSAGE, Logger::INFO);
-        $log->pushHandler($stream);
-        $log->info($message);
-        unset($log);
-        unset($stream);
-    }
-}
 
 /**
  * Wrapper to log Error Messages in a custom log file
